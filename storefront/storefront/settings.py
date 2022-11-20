3d61a4feebc36ee9603560c31e6bfabb0c0f4cb9
Nassor JA <rossan1tech@gmail.com>
2022-11-20 10:18:11 +0300

    Creating models

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'playground'
+    'playground',
+    'store',
+    'tags'
 ]
 
 MIDDLEWARE = [
